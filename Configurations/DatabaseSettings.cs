4f0c75c40ed7fbe0b6231aa979405a27417f16cd
Derek Claude <57004310+hack12-web@users.noreply.github.com>
2023-01-25 15:47:35 +0200

    Initial commit

---
+++ Configurations/DatabaseSettings.cs
@@ -0,0 +1,9 @@
+namespace DriversAppApi
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string CollectionName { get; set; } = null!;
+    }
+}
---
+++ Controllers/DriversController.cs
@@ -0,0 +1,58 @@
+using DriversAppApi.Models;
+using DriversAppApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DriversAppApi.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DriversController: ControllerBase
+    {
+        private readonly DriverService _driverService;
+        public DriversController(DriverService driverService)
+        {
+            _driverService = driverService;
+        }
+        [HttpGet("{id:length(24)}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var existingDriver = await _driverService.GetAsync(id);
+            if(existingDriver is null)
+                return NotFound();
+            return Ok(existingDriver);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var allDrivers = await _driverService.GetAsync();
+            if(allDrivers.Any())
+                return Ok(allDrivers);
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateDriver(Driver driver)
+        {
+            await _driverService.CreateAsync(driver);
+            return CreatedAtAction(nameof(Get), new{id = driver.Id}, driver);
+        }
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> PutDriver(string id, Driver driver)
+        {
+            var existingDriver = await _driverService.GetAsync(id);
+            if(existingDriver is null)
+                return BadRequest();
+            id = driver.Id;
+            await _driverService.UpdateAsync(driver);
+            return NoContent();
+        }
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> DeleteDriver(string id)
+        {
+            var existingDriver = await _driverService.GetAsync(id);
+            if(existingDriver is null)
+                return BadRequest();
+            await _driverService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
